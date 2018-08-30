f0e887e71dbf13aeee6bac8c6da4a4edb8729498
Awaymo <bhavik@awaymo.com>
2018-08-30 14:42:42 +0100

    commit

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,59 +1,18 @@
-import { StatusBar } from 'react-native'
 import { createStackNavigator } from 'react-navigation'
 
-import Home from '../screens/Home'
-import CurrencyList from '../screens/CurrencyList'
-import Options from '../screens/Options'
-import Themes from '../screens/Themes'
+import PassengerList from '../screens/PassengerList'
 
-const HomeStack = createStackNavigator(
+export default createStackNavigator(
   {
     Home: {
-      screen: Home,
+      screen: PassengerList,
       navigationOptions: {
         header: () => null,
         headerTitle: 'Home',
       },
     },
-    Options: {
-      screen: Options,
-      navigationOptions: {
-        headerTitle: 'Options',
-      },
-    },
-    Themes: {
-      screen: Themes,
-      navigationOptions: {
-        headerTitle: 'Themes',
-      },
-    },
   },
   {
     headerMode: 'screen',
-  },
-)
-
-const CurrencyListStack = createStackNavigator({
-  CurrencyList: {
-    screen: CurrencyList,
-    navigationOptions: ({ navigation }) => ({
-      headerTitle: navigation.state.params.title,
-    }),
-  },
-})
-
-export default createStackNavigator(
-  {
-    Home: {
-      screen: HomeStack,
-    },
-    CurrencyList: {
-      screen: CurrencyListStack,
-    },
-  },
-  {
-    mode: 'modal',
-    headerMode: 'none',
-    cardStyle: { paddingTop: StatusBar.currentHeight },
   },
 )
--- app/screens/PassengerList.js
+++ app/screens/PassengerList.js
@@ -1,48 +1,13 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
 import { Text, StatusBar, View } from 'react-native'
-import { connect } from 'react-redux'
 
-import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies'
+const PassengerList = ({ navigation, dispatch }) => (
+  <View style={{ flex: 1 }}>
+    <StatusBar translucent={false} barStyle="default" />
+    <View style={{ paddingTop: 20 }}>
+      <Text>Passenger List Screen</Text>
+    </View>
+  </View>
+)
 
-class CurrencyList extends Component {
-  static propTypes = {
-    navigation: PropTypes.object,
-    dispatch: PropTypes.func,
-  }
-
-  handlePress = (currency) => {
-    const { navigation, dispatch } = this.props
-    const { type } = navigation.state.params
-    if (type === 'base') {
-      dispatch(changeBaseCurrency(currency))
-    } else if (type === 'quote') {
-      dispatch(changeQuoteCurrency(currency))
-    }
-
-    navigation.goBack(null)
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <StatusBar translucent={false} barStyle="default" />
-        <Text>Passenger List Screen</Text>
-        {/* <FlatList
-          data={currencies}
-          renderItem={({ item }) => (
-            <ListItem
-              text={item}
-              selected={item === TEMP_CURRENT_CURRENCY}
-              onPress={() => this.handlePress(item)}
-            />
-          )}
-          keyExtractor={item => item}
-          ItemSeparatorComponent={Separator}
-        /> */}
-      </View>
-    )
-  }
-}
-
-export default connect()(CurrencyList)
+export default PassengerList
